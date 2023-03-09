e5a5f532bd2da4e2f811fa9f356ca7e8d643dedf
Nora Vazquez <vazquez.nora.g@gmail.com>
2023-03-09 11:51:51 -0600

    conexion bd2

---
+++ constructora/admin/config.php
@@ -0,0 +1,9 @@
+<?php
+    //DATA BASE
+    define("DBDRIVER", "mysql");
+    define("DBHOST", "127.0.0.1");
+    define("DBNAME", "constructora");
+    define("DBUSER", "constructora");
+    define("DBPASS", "1234");
+    define("DBPORT", "3306");
+?>
---
+++ constructora/admin/test.php
@@ -0,0 +1,9 @@
+<?php
+    require_once('config.php');
+    $connectionString = DBDRIVER':host='.DBHOST.';dbname='.DBNAME.';port='.DBPORT;
+    $db = new PDO($connectionString, DBUSER, DBPASS);
+    $prep2 = $db->prepare("select * from departamento");
+    $prep2->execute();
+    $result = $prep2->fetchAll();
+    print_r($result);
+?>
